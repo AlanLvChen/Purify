365a4eac1e41ba96ef603025064de5c9bb54468f
AlanLvChen <alan.codemonkey@gmail.com>
2022-02-08 19:37:16 -0800

    Added landingPage

---
+++ Purify/frontend/public/static/landingPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../css/landingPage.css';
+
+const landingPage = () => {
+    return (
+        <div className="title disable-select">
+            <h1>Purify</h1>
+            <p>Simple. Intuitive.</p>
+            <p>Organize your Spotify playlists</p>
+            <button type="button" className="btn btn-green btn-lg" onClick={() => console.log("Success")}>Get Started</button>
+            
+        </div>
+        
+    );
+};
+
+export default landingPage;
--- Purify/frontend/src/App.js
+++ Purify/frontend/src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import LandingPage from '../public/static/landingPage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button type="button" className="btn btn-success">Success</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <LandingPage />
     </div>
   );
 }
