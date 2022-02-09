70f5469ffffc43ef280a308621b58ad9272542b6
AlanLvChen <alan.codemonkey@gmail.com>
2022-02-08 22:54:08 -0800

    added landingpage

--- Purify/frontend/src/App.js
+++ Purify/frontend/src/App.js
@@ -1,5 +1,4 @@
-import './App.css';
-import LandingPage from '../public/static/landingPage'
+import LandingPage from './static/landingPage'
 
 function App() {
   return (
--- Purify/frontend/src/static/landingPage.js
+++ Purify/frontend/src/static/landingPage.js
@@ -4,9 +4,9 @@ import '../css/landingPage.css';
 const landingPage = () => {
     return (
         <div className="title disable-select">
-            <h1>Purify</h1>
-            <p>Simple. Intuitive.</p>
-            <p>Organize your Spotify playlists</p>
+            <h1 className="landing-header">Purify</h1>
+            <p className="landing-p">Simple.  Intuitive.</p>
+            <p className="landing-p">Organize your Spotify playlists</p>
             <button type="button" className="btn btn-green btn-lg" onClick={() => console.log("Success")}>Get Started</button>
             
         </div>
